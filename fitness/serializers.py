63a4950c4fb8699c14bfc95754e50db8ed8caeb6
saybube <ebubenwamaka@gmail.com>
2026-03-05 08:32:36 -0500

    created serializers for models and views in fitness app

---
+++ fitness/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Activity, FoodLog, WeightLog
+
+class ActivitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Activity
+        fields = ['id', 'activity_type', 'duration_minutes', 'distance_km', 'calories_burned', 'date']
+        
+class FoodLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FoodLog
+        fields = ['id', 'meal_type', 'food_item', 'calories_ingested', 'date']
+
+class WeightLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WeightLog
+        fields = ['id', 'weight_kg', 'date']
