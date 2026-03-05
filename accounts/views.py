75ba6775d88668c4389ba87aa2b312c19ae1e6b7
saybube <ebubenwamaka@gmail.com>
2026-03-05 09:49:46 -0500

    created register and login endpoints in accounts and updated fitflow url with the pathway

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework import generics, permissions
+from .serializers import UserSerializer
+from .models import User
 # Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
--- fitflow_project/urls.py
+++ fitflow_project/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from accounts.views import RegisterView
+from rest_framework.authtoken.views import obtain_auth_token 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('fitness.urls')),
+    path('api/register/', RegisterView.as_view(), name='register'),
+    path('api/login/', obtain_auth_token, name='login'),
 ]
